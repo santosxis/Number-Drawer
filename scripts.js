babe9c4abef4fd1291956e5743a3ac968275129b
santosxis <kassioxis@icloud.com>
2025-05-05 12:01:26 -0300

    Incrementado alguns operadores no código js

--- scripts.js
+++ scripts.js
@@ -1,15 +1,22 @@
 function generateNumber() {
 
     /* console.log('Me chamou?') */
- 
-     const min = Math.ceil(document.querySelector('.input-min').value) /* .value - Valor dentro do input*/
-     const max = Math.floor(document.querySelector('.input-max').value) /* .value - Valor dentro do input*/
- 
-     /*  console.log(min,max) */
- 
-     const result = Math.floor(Math.random() * (max - min + 1)) + min;
- 
-     alert (result)
- }
- 
- 
+
+    const min = Math.ceil(document.querySelector('.input-min').value) /* .value - Valor dentro do input*/
+    const max = Math.floor(document.querySelector('.input-max').value) /* .value - Valor dentro do input*/
+
+    /*  console.log(min,max) */
+
+    if (min >= max) {
+        alert('O valor Mínimo tem que ser MENOR que o valor Máximo')
+    } 
+    
+    else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+        alert(result)
+    }
+
+
+}
+
